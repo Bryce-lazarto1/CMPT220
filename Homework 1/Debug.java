f17e025b42188fbbc8df06ddfa8b1b7adeeaeeeb
Bryce-lazarto1 <143512529+Bryce-lazarto1@users.noreply.github.com>
2024-09-10 13:31:21 -0400

    Update Debug.java

--- Homework 1/Debug.java
+++ Homework 1/Debug.java
@@ -7,22 +7,29 @@ public class Debug {
         Scanner sc = new Scanner(System.in);
         //here's a good working example for you to reference!
         //first: we ask our user for our prompt
-        System.out.println("What is your name? ");
+       /// System.out.println("What is your name? ");
         //second: we create a string variable and store the user input back to it
-        String name = sc.nextLine();
+        ////String name = sc.nextLine();
         //third: we print it back out!
-        System.out.println("Hello, " + name + "! ");
-        
+        ///System.out.println("Hello, " + name + "! ");///
 
+        
         //finish this one for me by receiving and printing the user's age back to them
         System.out.println("How old are you? ");
+
+        String number = sc.nextLine();
+
+        System.out.println("You are " + number +  " Years old! ");
+
+
 
 
         //can you do some math for me? take these variable and print out the sum of them!
         int num1 = 10;
         int num2 = 37;
         //?
-
+        System.out.println(num1+ num2 );
+    
         
 
     }
