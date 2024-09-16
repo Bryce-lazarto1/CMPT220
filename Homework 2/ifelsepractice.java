4564a50b070c004fb9603e803aec877020f21a49
Bryce-lazarto1 <143512529+Bryce-lazarto1@users.noreply.github.com>
2024-09-15 23:02:28 -0400

    Update ifelsepractice.java

--- Homework 2/ifelsepractice.java
+++ Homework 2/ifelsepractice.java
@@ -10,10 +10,36 @@ import java.util.Scanner;
 * */
 public class Main {
     public static void main(String[] args) {
-
-
-
+        Scanner sc = new Scanner(System.in);
+        int score = 0;    
+        System.out.println("whats 4+2");
+        int answer1 = sc.nextInt(); 
+        if (answer1 == 6){
+            System.out.println("correct!");
+            System.out.println((++score) + " is your score");
+       }else{
+            System.out.println("Wrong!");
+            System.out.println(score + " is your score");
+       
+        } 
+        System.out.println("whats 5+6");
+        int answer2 = sc.nextInt(); 
+        if (answer2 == 11){
+            System.out.println("correct!");
+            System.out.println((++score) + " is your score");
+       }else{
+            System.out.println("Wrong!");
+            System.out.println( score + " is your score");
+        }
+        System.out.println("whats 8+2");
+        int answer3 = sc.nextInt(); 
+        if (answer3 == 10){
+            System.out.println("correct!");
+            System.out.println((++score) + " is your score");
+       }else{
+            System.out.println("Wrong!");
+            System.out.println(score + "is your score");
+        }
     }
 }
-
 
