4b4938f568c6ea3b363f3fc7f8701cfa456300da
Bryce-lazarto1 <143512529+Bryce-lazarto1@users.noreply.github.com>
2024-11-09 12:55:51 -0500

    Update ArrayDebug.java

--- Homework 4/ArrayDebug.java
+++ Homework 4/ArrayDebug.java
@@ -4,11 +4,11 @@ public class Main {
     public static void main(String[] args) {
 
         //Create me an array named "nameArray" that is a string and has 4 values in it, give me 4 names of people or animals you care about
-
+        String[] nameArray = {"bryce", "john", "jack", "lebron"};
         //Create me an array without any values, but it's an integer array named "numArray"
-
+        int[] numArray;
         //Create me an int array named "newArray" and set it to have 20 null spaces
-
+        int[] newArray = new int[20];   
 
 
 
@@ -16,24 +16,31 @@ public class Main {
         //Here's our first practice array!
         String[] sortMe = {"Apple","Kiwi","Orange","Banana","Watermelon","Strawberry"};
        //What method is missing here to actually sort our array?
-        Arrays.?(sortMe);
+        Arrays.sort(sortMe);
         //And what method is missing here to print out that array?
-        System.out.println(?);
+        System.out.println(Arrays.toString(sortMe));
         //After this, you cannot use the array helper file, gotta go back to the old ways!!
 
         //another one!
         int addMe[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         //How would we add every index together and print out the sum??
-        //A small hint:
-        int sum = 0;
-        for(?;?;?){
-
+          int sum = 0;
+        for (int i = 0; i < addMe.length; i++) {
+            sum += addMe[i];
         }
+        System.out.println(sum);
+    }
+}
         
         //thinking one! here's an array
         int maxMe[] = {280, 4892, 134, 23, 1937, 5329, 932, 89};
         //What would you have to do to find the largest number and then print that value?
-        
-
+        int Biggest = maxMe[0];
+        for (int i = 1; i < maxMe.length; i++) {
+            if (maxMe[i] > Biggest) {
+                Biggest = maxMe[i]; 
+            }
+        }
+        System.out.println( Biggest + "is the Biggest");
     }
 }
