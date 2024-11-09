6f62b600c98a84f4d8a328d71156b728d6870c47
Bryce-lazarto1 <143512529+Bryce-lazarto1@users.noreply.github.com>
2024-11-09 12:32:14 -0500

    Update Array.java

--- Homework 4/Array.java
+++ Homework 4/Array.java
@@ -12,5 +12,20 @@ public class Main {
         //Don't let the size of this scare you! no matter how big an array is, it all works the same!
         int[] myArray = {10,3,295,38,20,3,4,267,2445,10,5566,87,93,17,10,2,87,267,3176,3,82};
         //you cannot use the array util. Do this one by hand :(
+        HashMap<Integer, Integer> numberCounts = new HashMap<>();
+         for (int i = 0; i < myArray.length; i++) {
+            int num = myArray[i]; 
+            if (counts.containsKey(num)) {
+                int liveCount = counts.get(num);
+                counts.put(num, liveCount + 1);
+            } else {
+                counts.put(num, 1);
+            }
+        }
+        for (int num : counts.keySet()) {
+            int count = counts.get(num);
+            if (count > 1) {
+                System.out.println(num + " shows up  "+ count + " times.");
+            }
+        }
     }
-}
