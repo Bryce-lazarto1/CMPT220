5edcb9b97a6b6b3f3f4a2a271e1820dc029ee035
Bryce-lazarto1 <143512529+Bryce-lazarto1@users.noreply.github.com>
2024-11-21 13:40:36 -0500

    Update functions.java

--- Homework 5/functions.java
+++ Homework 5/functions.java
@@ -7,7 +7,22 @@ feel free to google some inspiration though :)
 
 public class Main {
     public static void main(String[] args) {
-    
-    }
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number to see if its the same number reversed");
+        int num = sc.nextInt();
+         if (isSame(num)) {
+            System.out.println(num + " is the same reversed");
+        }else {
+            System.out.println(number + " is not the same reversed");
    
-}
+    public static boolean isSame(int num) {
+        int usernum = num;
+        int reversed = 0;
+        while (num > 0) {
+            int digit = num % 10; 
+            reversed = reversed * 10 + digit;
+            num /= 10;
+        }
+        return usernum == reversed;
+        }
+    }
